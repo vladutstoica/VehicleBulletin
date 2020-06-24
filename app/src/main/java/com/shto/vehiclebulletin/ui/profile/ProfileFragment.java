2f83c9ce35579114053dc8d85a0e317bde9ef6fb
SHTO <stoicavladut18@gmail.com>
2020-06-24 21:04:42 +0300

    profile retrieve data from viewmodel

--- app/src/main/java/com/shto/vehiclebulletin/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/shto/vehiclebulletin/ui/profile/ProfileFragment.java
@@ -1,7 +1,6 @@
 package com.shto.vehiclebulletin.ui.profile;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,27 +15,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.shto.vehiclebulletin.R;
 
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = "profile";
     private ProfileViewModel mProfileViewModel;
-    private FirebaseFirestore db;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Declare an instance of FirebaseFirestore
-        db = FirebaseFirestore.getInstance();
-    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -46,7 +32,7 @@ public class ProfileFragment extends Fragment {
         final TextView textView = root.findViewById(R.id.text_notifications);
 
         mProfileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
-        mProfileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        mProfileViewModel.getProfileName().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
@@ -70,28 +56,10 @@ public class ProfileFragment extends Fragment {
                 Toast.makeText(getContext(), "email verified", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(getContext(), "email unverified", Toast.LENGTH_SHORT).show();
+                // TODO: make snackbar work and open email for email verification
+                // Snackbar.make(getActivity().getCurrentFocus(), "Email Unverified", Snackbar.LENGTH_SHORT).show();
             }
         }
-
-        db.collection("users")
-                .document(user.getUid())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                Log.d(TAG, "DocumentSnapshot data: " + document.getData().get("name").toString());
-
-                            } else {
-                                Log.d(TAG, "No such document");
-                            }
-                        } else {
-                            Log.d(TAG, "get failed with ", task.getException());
-                        }
-                    }
-                });
 
         return root;
     }
--- app/src/main/java/com/shto/vehiclebulletin/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/shto/vehiclebulletin/ui/profile/ProfileViewModel.java
@@ -1,16 +1,54 @@
 package com.shto.vehiclebulletin.ui.profile;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ProfileViewModel extends ViewModel {
 
+    private static final String TAG = "viewmodel";
     private MutableLiveData<String> mText;
 
     public ProfileViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
+        //mText.setValue("This is profile fragment");
+    }
+
+    // TODO: remove firebase from viewmodel and create a repository for firebase
+    public LiveData<String> getProfileName() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        db.collection("users")
+                .document(user.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData().get("name").toString());
+                                mText.setValue(document.getData().get("name").toString());
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+
+        return mText;
     }
 
     public LiveData<String> getText() {
